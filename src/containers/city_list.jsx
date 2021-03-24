b3f2529b6b5e341c37e21821303b67bff6654aa7
Vinicius Libero <libero.moreira@gmail.com>
2021-03-24 18:14:02 -0300

    promoted city_list to container

---
+++ src/containers/city_list.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+// Component or Container and Action
+import { setCities } from '../actions';
+import City from './city.jsx';
+
+class FlatList extends Component {
+
+  componentWillMount() {
+    this.props.setCities();
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.cities.map((city) => {
+          return <City key={city.name} city={city} />
+        })}
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setCities: setCities }, dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    cities: state.cities
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
