c6268bc216cf4fed4fe0ff3b244c1197b0af8e72
Vinicius Libero <libero.moreira@gmail.com>
2021-03-24 18:39:23 -0300

    included ActiveCity in App and removed cities data

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CityList from '../containers/city_list.jsx';
-import { cities } from '../../data/cities.js';
+import ActiveCity from '../containers/active_city';
+// import { cities } from '../../data/cities.js';
 
 
 const App = () => {
   return (
     <div className="app">
       <CityList cities={cities} />
+      <ActiveCity />
     </div>
   );
 };
