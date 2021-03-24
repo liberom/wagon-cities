f79da2f41f664687949bfe53286cf5a5386505ed
Vinicius Libero <libero.moreira@gmail.com>
2021-03-24 17:43:51 -0300

    edited actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import cities from '../../data/cities.js';
+
 // TODO: add and export your own actions
+export const SET_CITIES = 'SET_CITIES';
+// export const SELECT_CITY = 'SELECT_CITY';
+
+export function setCities() {
+  return {
+    type: SET_CITIES,
+    payload: cities
+  };
+}
+
+// export function selectCity(city) {
+//   return {
+//     type: SELECT_CITY,
+//     payload: city
+//   };
+// }
