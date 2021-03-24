0e0dae42e45d621c00eb8a313bb06034addf5ef7
Vinicius Libero <libero.moreira@gmail.com>
2021-03-24 18:41:15 -0300

    edited index.jsx to include ActiveCityReducer

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 
 import citiesReducer from './reducers/cities_reducer';
+import activeCityReducer from './reducers/active_city_reducer';
 
 // internal modules
 import App from './components/app';
@@ -12,7 +13,8 @@ import '../assets/stylesheets/application.scss';
 
 // State and reducers
 const reducers = combineReducers({
-  cities: citiesReducer
+  cities: citiesReducer,
+  activeCity: activeCityReducer
 });
 
 // render an instance of the component in the DOM
